d376cdce79c2d9ce8043a946035b1c1fba834ba0
Denis Loginov <git@dinvlad.dev>
2025-08-27 15:20:03 -0400

    Change Omarchy repo to GH Pages mirror

--- install/preflight/repositories.sh
+++ install/preflight/repositories.sh
@@ -10,7 +10,7 @@ fi
 
 # Add the Omarchy repository as first choice
 if ! grep -q "omarchy" /etc/pacman.conf; then
-  sudo sed -i '/^\[core\]/i [omarchy]\nSigLevel = Optional TrustAll\nServer = https:\/\/pkgs.omarchy.org\/$arch\/\n' /etc/pacman.conf
+  sudo sed -i '/^\[core\]/i [omarchy]\nSigLevel = Optional TrustAll\nServer = https:\/\/dinvlad.github.io\/armarchy/\/$arch\/\n' /etc/pacman.conf
 fi
 
 # Set mirrors to global ones only
